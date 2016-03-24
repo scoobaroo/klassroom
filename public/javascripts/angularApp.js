66c59048af80f2b60a4bacd3f61226af212cf515
Marcel Degas <marcel.degas@gmail.com>
2016-03-23 22:48:09 -0700

    stripped away more of the crap from yeoman

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-// Declare app level module which depends on filters, and services
+// Declares an app level module which depends on filters, and services
 angular.module('klassroomApp', [
   'ngRoute',
   'klassroom.services',
