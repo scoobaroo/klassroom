2623607c34f886ad6e99968283b728b8c861ff4f
Marcel Degas <marcel.degas@gmail.com>
2016-03-31 14:03:34 -0700

    everything working again after a big clean-up

--- server.js
+++ server.js
@@ -3,23 +3,31 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./app/routes/index');
 var app = express();
+var db = require("./config/db");
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(logger('dev'));
+app.set('views', path.join(__dirname, 'public/views'));
+// why would I need to include __dirname in here? 
+app.use(express.static('public'));
+
 app.use(bodyParser.json());
+// do i need to change this to be true? 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
-app.use('/users', users);
 
 
+var port = process.env.PORT || 8000;
 
+// connect to our mongoDB database 
+// (uncomment after you enter in your own credentials in config/db.js)
+// mongoose.connect(db.url); 
+
+app.listen(port);
+console.log('The magic is happening on ' + port);
 
 
 module.exports = app;
