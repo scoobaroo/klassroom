bc439b06634fbe47351e84dbca832488ab0453ad
Marcel Degas <marcel.degas@gmail.com>
2016-04-04 16:10:50 -0700

    removed stormpath stuff for the heroku build

--- public/js/angularApp.js
+++ public/js/angularApp.js
@@ -5,17 +5,14 @@
 angular.module('klassroomApp', [
   'ngRoute',
   'klassroom.services',
-  'klassroom.controllers', 
-  'ui.router'
-  'stormpath', 
-  'stormpath.templates'
+  'klassroom.controllers'
 ]).
-config(['$urlRouteProvider', function($urlRouteProvider) {
-  $urlRouteProvider.when('/splash', {templateUrl: '/partials/splash.html', controller: 'SplashController'});
-  $urlRouteProvider.when('/criteria', {templateUrl: '/partials/criteria.html', controller: 'CriteriaController'});
-  $urlRouteProvider.when('/students', {templateUrl: '/partials/students.html', controller: 'StudentsController'});
-  $urlRouteProvider.when('/generate', {templateUrl: '/partials/generate.html', controller: 'GenerateController'});
-  $urlRouteProvider.when('/details', {templateUrl: '/partials/details.html', controller: 'DetailsController'});
-  $urlRouteProvider.when('/contact', {templateUrl: '/partials/contact.html', controller: 'ContactController'});
-  $urlRouteProvider.otherwise({redirectTo: '/splash'});
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/splash', {templateUrl: '/partials/splash.html', controller: 'SplashController'});
+  $routeProvider.when('/criteria', {templateUrl: '/partials/criteria.html', controller: 'CriteriaController'});
+  $routeProvider.when('/students', {templateUrl: '/partials/students.html', controller: 'StudentsController'});
+  $routeProvider.when('/generate', {templateUrl: '/partials/generate.html', controller: 'GenerateController'});
+  $routeProvider.when('/details', {templateUrl: '/partials/details.html', controller: 'DetailsController'});
+  $routeProvider.when('/contact', {templateUrl: '/partials/contact.html', controller: 'ContactController'});
+  $routeProvider.otherwise({redirectTo: '/splash'});
 }]);
