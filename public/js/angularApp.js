692214183eea7cc277e18a520b1121bcffef5865
Marcel Degas <marcel.degas@gmail.com>
2016-04-08 16:58:01 -0700

    changed to ui router

--- public/js/angularApp.js
+++ public/js/angularApp.js
@@ -2,17 +2,44 @@
 
 
 // Declares an app level module which depends on filters, and services
-angular.module('klassroomApp', [
+var klassroomApp = angular.module('klassroomApp', [
   'ngRoute',
   'klassroom.services',
-  'klassroom.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/splash', {templateUrl: '/partials/splash.html', controller: 'SplashController'});
-  $routeProvider.when('/criteria', {templateUrl: '/partials/criteria.html', controller: 'CriteriaController'});
-  $routeProvider.when('/students', {templateUrl: '/partials/students.html', controller: 'StudentsController'});
-  $routeProvider.when('/generate', {templateUrl: '/partials/generate.html', controller: 'GenerateController'});
-  $routeProvider.when('/details', {templateUrl: '/partials/details.html', controller: 'DetailsController'});
-  $routeProvider.when('/contact', {templateUrl: '/partials/contact.html', controller: 'ContactController'});
-  $routeProvider.otherwise({redirectTo: '/splash'});
-}]);
+  'klassroom.controllers', 
+  'ui.router'
+]);
+
+klassroomApp.config(function($stateProvider, $urlRouterProvider) {
+  
+  $urlRouterProvider.otherwise('/splash');
+  
+  $stateProvider
+    // where do i specify which controller controls which partial? 
+    .state('splash', {
+      url: '/splash', 
+      templateUrl: '/partials/splash.html'
+    })
+    .state('criteria', {
+      url: '/criteria', 
+      templateUrl: '/partials/criteria.html'
+    })
+    .state('/students', {
+      url: '/students', 
+      templateUrl: '/partials/students.html'
+    })
+    .state('generate', {
+      url: '/generate', 
+      templateUrl: '/partials/generate.html'
+    })
+    .state('details', {
+      url: '/details', 
+      templateUrl: '/partials/details.html'
+    })
+});
+
+// $u('/splash', {templateUrl: '/partials/splash.html', controller: 'SplashController'});
+  // $urlRouterProvider.when(, {templateUrl: , controller: 'CriteriaController'});
+  // $urlRouterProvider.when(, {templateUrl: , controller: 'StudentsController'});
+  // $urlRouterProvider.when('/generate', {templateUrl: '/partials/generate.html', controller: 'GenerateController'});
+  // $urlRouterProvider.when('/details', {templateUrl: '/partials/details.html', controller: 'DetailsController'});
+  // $urlRouterProvider.when('/contact', {templateUrl: '/partials/contact.html', controller: 'ContactController'});
