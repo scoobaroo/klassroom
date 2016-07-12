92d87f66f64c3a2461eaacd33b441188f406ec7d
scoobaroo <scoobaroo415@gmail.com>
2016-07-11 20:28:36 -0700

    latest update

--- config/db.js
+++ config/db.js
@@ -1,1 +1,7 @@
-module.exports = {}
+module.exports = {
+  // App Settings
+  MONGO_URI: process.env.MONGO_URI || 'localhost',
+  TOKEN_SECRET: process.env.TOKEN_SECRET || 'YOUR_UNIQUE_JWT_TOKEN_SECRET',
+  // OAuth 2.0
+  FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || 'b8bd1618fae9da477f1e48b1be75bd49',
+};
